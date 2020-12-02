e1f35e05ec23bfe1fbd639c0f344e5c1991321f4
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:56:58 +0000

    implementing one function that is nesting all the other functions

--- iss.js
+++ iss.js
@@ -8,63 +8,26 @@
  */
 const request = require('request');
 const url = 'https://api.ipify.org/?format=json';
-const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  request(url, (error, response, body) => {
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
     if (error) {
-      callback(error, null);
-      return;
+      return callback(error, null);
     }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-  
-    const data = JSON.parse(body);
-    if (data.ip) {
-      let myIp = data.ip;
-      callback(null, myIp);
-    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
   });
 };
 
-const fetchCoordsByIP = function(myUrl, callback) {
-  request(myUrl, (error, response, body) => {
-    if (error) {
-      callback (error, null);
-      return;
-    }
-   
-    const { latitude, longitude } = JSON.parse(body).geo;
-    callback(null, { latitude, longitude });
-    
-  });
-
-}
-const startApi = 'http://api.open-notify.org/iss-pass.json';
-/**
- * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
- * Input:
- *   - An object with keys `latitude` and `longitude`
- *   - A callback (to pass back an error or the array of resulting data)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The fly over times as an array of objects (null if error). Example:
- *     [ { risetime: 134564234, duration: 600 }, ... ]
- */
-
-const fetchISSFlyOverTimes = function(coords, callback) {
-  request( `${startApi}?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    const data = JSON.parse(body).response;
-    if (data) {
-      callback(null, data);
-    }
-  })
-};
-
-module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
+module.exports = {nextISSTimesForMyLocation};
