b32130bb33e441031d990fb4fc55b1f6062d7196
saoussen <sawsan_sli3i@live.fr>
2020-12-02 18:30:20 +0000

    added statusCode to  iss.js

--- iss.js
+++ iss.js
@@ -15,6 +15,12 @@ const fetchMyIP = function(callback) {
       callback(error, null); 
       return;
     }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+  
     const data = JSON.parse(body);
     if (data.ip) {
       let myIp = data.ip;
