e732ed994181f8bd3bb1c484a935acb2052fc80d
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:56:58 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const {nextISSTimesForMyLocation} = require('./iss');
-const myUrl = `https://api.ipgeolocationapi.com/geolocate/72.141.69.246`;
-const Coords = { latitude: '49.27670', longitude: '-123.13000' };
+// index.js
 
-fetchISSFlyOverTimes(Coords, (error, passTimes) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const  nextISSTimesForMyLocation  = require('./iss');
 
-  console.log('It worked! Returned flyover times:' , passTimes);
-});
-fetchCoordsByIP(myUrl, (error, coords) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log('here is your url' , coords);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+
+
 });
 
