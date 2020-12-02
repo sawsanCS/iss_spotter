682439e35b4a22958e614d6b9153ea20e11d6ecb
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:56:58 +0000

    added fetchCoordsByIP to iss_promised.js

--- iss_promised.js
+++ iss_promised.js
@@ -2,5 +2,9 @@ const request = require('request-promise-native');
 const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://api.ipgeolocationapi.com/geolocate/${ip}`);
+};
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
