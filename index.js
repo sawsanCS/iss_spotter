733cf8dbf30e8398ad7c6115ec574012c2decd59
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:19:58 +0000

    called fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
-let myUrl = 'https://ipvigilante.com/72.141.69.246';
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
+let myUrl = 'https://ipvigilante.com/json/72.141.69.246';
+const Coords = { latitude: '49.27670', longitude: '-123.13000' };
 
-fetchCoordsByIP(myUrl, (error, url) => {
+fetchISSFlyOverTimes(Coords, (error, passTimes) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+fetchCoordsByIP(myUrl, (error, coords) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-  console.log('here is your url' , url);
+  console.log('here is your url' , coords);
 });
+
