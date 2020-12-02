c2251437a930a59130bf23dfde99ce75d94b9e9e
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:56:58 +0000

    implemented function fetchISSFlyOverTimes

--- iss_promised.js
+++ iss_promised.js
@@ -6,5 +6,11 @@ const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
   return request(`https://api.ipgeolocationapi.com/geolocate/${ip}`);
 };
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body).geo;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
