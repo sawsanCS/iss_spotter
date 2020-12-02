d61fb45b979df8bb21851c7907e52a90304af525
saoussen <sawsan_sli3i@live.fr>
2020-12-02 19:56:58 +0000

    imported function fetchISSFlyOverTimes

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
 fetchMyIP()
   .then(fetchCoordsByIP)
-  .then(body => console.log(body));
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(JSON.parse(body).response));
   
